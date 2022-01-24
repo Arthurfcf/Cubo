07328bbd593153f00888636de0df5b24a9430e7e
Arthur Fernando Carvalho Ferreira <arthur.ferreira@compasso.com.br>
2022-01-24 10:13:58 -0300

    Classe controller recriada

--- BackEnd/Cubo.API/Controllers/AlgarismoRomanoController.cs
+++ BackEnd/Cubo.API/Controllers/AlgarismoRomanoController.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Cubo.API.ViewModels;
+using Cubo.Domain.Interfaces;
+using Cubo.Domain.Interfaces.Repository;
+using Cubo.Domain.Interfaces.Service;
+using Cubo.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Cubo.Domain.Interfaces.Repository;
-using Cubo.Core.Repositories;
-using Cubo.Domain.Interfaces;
-using Cubo.Domain.Models;
-using Cubo.Domain.Interfaces.Service;
-using AutoMapper;
-using Cubo.API.ViewModels;
 
 namespace Cubo.API.Controllers
 {
@@ -18,37 +18,36 @@ namespace Cubo.API.Controllers
     public class AlgarismoRomanoController : MainController
     {
 
-        private readonly IAlgarismoRomanoRepository _algarismoRomanoRepository ;
+        private readonly IAlgarismoRomanoRepository _algarismoRomanoRepository;
         private readonly IAlgarismoRomanoService _algarismoRomanoService;
         private readonly IMapper _mapper;
 
-        public AlgarismoRomanoController(IAlgarismoRomanoService algarismoRomanoService,
-                                             INotificator notificator, 
-                                             IAlgarismoRomanoRepository algarismoRomanoRepository,
-                                             IMapper mapper, IUser user) : base(notificator, user)
+        public AlgarismoRomanoController(INotificator notificator,
+                                 IAlgarismoRomanoRepository algarismoRomanoRepository,
+                                 IAlgarismoRomanoService algarismoRomanoService,
+                                 IMapper mapper,
+                               IUser user) : base(notificator, user)
         {
             _algarismoRomanoRepository = algarismoRomanoRepository;
             _algarismoRomanoService = algarismoRomanoService;
             _mapper = mapper;
         }
-
-
+        [Authorize]
         [HttpGet]
         public async Task<IEnumerable<AlgarismoRomanoViewModel>> GetAll()
         {
             return _mapper.Map<IEnumerable<AlgarismoRomanoViewModel>>(await _algarismoRomanoRepository.GetAll());
-
         }
+
+        [Authorize]
         [HttpPost]
-        public async Task<ActionResult<AlgarismoRomanoViewModel>> Add(AlgarismoRomanoViewModel algarismoRomanoView)
+        public async Task<ActionResult<AlgarismoRomanoViewModel>> Add(AlgarismoRomanoViewModel algarismoRomanoViewModel)
         {
             if (!ModelState.IsValid) return CustomResponse(ModelState);
-           
-             await _algarismoRomanoService.Add(_mapper.Map<AlgarismoRomano>(algarismoRomanoView));
 
-            return CustomResponse(algarismoRomanoView);
-          
+            await _algarismoRomanoService.Add(_mapper.Map<AlgarismoRomano>(algarismoRomanoViewModel));
+
+            return CustomResponse(algarismoRomanoViewModel);
         }
-
     }
 }
